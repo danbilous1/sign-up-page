06b20b328ed7504baf5b73e5d240653fc23a8687
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2024-12-17 23:08:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { useState } from "react";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">home</Link>
+        {!token ? (
+          <Link to="/register">register</Link>
+        ) : (
+          <Link to="/profile">profile</Link>
+        )}
+      </nav>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register setToken={setToken} />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
